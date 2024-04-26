5de490dd69de29c16c2aa16657d033a4824d0203
deverson-freitas <dewesonfreitas00@gmail.com>
2024-04-26 16:16:53 -0300

    resolução dos exwercícios

---
+++ index.ts
@@ -0,0 +1,104 @@
+const fs = require("fs");
+
+const lerArquivo = (): unknown => {
+  return JSON.parse(fs.readFileSync("./bd.json"));
+};
+
+const escreverArquivo = (dados: any): void => {
+  fs.writeFileSync("./bd.json", JSON.stringify(dados));
+};
+
+type Endereco = {
+  cep: string;
+  rua: string;
+  complemento?: string;
+  bairro: string;
+  cidade: string;
+};
+
+type Usuario = {
+  nome: string;
+  email: string;
+  cpf: string;
+  profissao?: string;
+  endereco: Endereco | null;
+};
+
+const cadastrarUsuario = (dados: Usuario): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  bd.push(dados);
+  escreverArquivo(bd);
+  return dados;
+};
+
+const listarUsuarios = (): Usuario[] => {
+  return lerArquivo() as Usuario[];
+};
+
+const detalharUsuario = (cpf: string): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("usuario não encontrado");
+  }
+
+  return usuario;
+};
+
+const atualizarUsuario = (cpf: string, dados: Usuario) => {
+  const bd = lerArquivo() as Usuario[];
+
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("usuario não encontrado");
+  }
+
+  Object.assign(usuario, dados);
+
+  escreverArquivo(bd);
+
+  return dados;
+};
+
+const excluirUsuario = (cpf: string): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("usuario não encontrado");
+  }
+
+  const exclusao = bd.filter((usuario) => {
+    return usuario.cpf === cpf;
+  });
+
+  escreverArquivo(exclusao);
+
+  return usuario;
+};
+
+const listarUsuarioss = (filtro?: string): Usuario[] => {
+  const bd = lerArquivo() as Usuario[];
+
+  const usuarios = bd.filter((usuario) => {
+    if (filtro) {
+      return usuario.profissao === filtro;
+    }
+    return usuario;
+  });
+
+  return usuarios;
+};
+
+const bd = lerArquivo();
+console.log(bd);
